a051b0a10352a3e037675671b33e02763d395b23
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2018-04-08 23:46:29 -0700

    add nfd/daemon/fw/ddos-helper.hpp

---
+++ daemon/fw/ddos-helper.hpp
@@ -0,0 +1,62 @@
+#ifndef NFD_DAEMON_FW_DDOS_HELPER_HPP
+#define NFD_DAEMON_FW_DDOS_HELPER_HPP
+
+#include "../../../utils/ndn-ns3-packet-tag.hpp"
+#include "../../../utils/ndn-ns3-cc-tag.hpp"
+#include "../../../../core/model/ptr.h"
+#include "mt-forwarding-info.hpp"
+#include "algorithm.hpp"
+
+namespace nfd {
+namespace fw {
+
+using ndn::Name;
+
+class DdosHelper
+{
+public:
+  // @return the number of pending interests with the same prefix
+  // @param the Interest name
+  // @param prefix length (component number). Default: FIB entry size or + 2?
+  // @note help to calculate the traffic change
+  static uint32_t
+  getInterestNumberWithPrefix(Forwarder& forwarder,
+                              const Name& interestName,
+                              const int length)
+  {
+    return 0;
+  }
+
+  // @return the current PIT table usage rate
+  static double
+  getCurrentPitTableUsage(Forwarder& forwarder)
+  {
+    return 0.0;
+  }
+
+  // @return the current prefix success ratio: suc/all in 1 second
+  // @param TODO
+  // @param the Interest name
+  // @param prefix length (component number). Default: FIB entry size
+  static double
+  getPrefixSuccessRatio(const Name& interestName,
+                        const int length)
+  {
+    return 0.0;
+  }
+
+  // @return whether is under DDoS or not, decided by decision machine
+  // @param TODO
+  static bool
+  isUnderDDoS()
+  {
+    return false;
+  }
+
+};
+
+
+} // namespace fw
+} // namespace nfd
+
+#endif // NFD_DAEMON_FW_DDOS_HELPER_HPP
--- daemon/fw/str-helper.hpp
+++ daemon/fw/str-helper.hpp
@@ -1,7 +1,6 @@
 #ifndef NFD_DAEMON_FW_STRATEGY_HELPER_HPP
 #define NFD_DAEMON_FW_STRATEGY_HELPER_HPP
 
-//#include "../../common.hpp"
 #include "../../../utils/ndn-ns3-packet-tag.hpp"
 #include "../../../utils/ndn-ns3-cc-tag.hpp"
 #include "../../../../core/model/ptr.h"
@@ -11,17 +10,16 @@
 namespace nfd {
 namespace fw {
 
-using ndn::Name; 
+using ndn::Name;
 
 class StrHelper
 {
-
 public:
 
   static void
   reduceFwPerc(shared_ptr<MtForwardingInfo> forwInfo,
-      const FaceId reducedFaceId,
-      const double change)
+               const FaceId reducedFaceId,
+               const double change)
   {
 
     if (forwInfo->getFaceCount() == 1) {
@@ -38,7 +36,7 @@ public:
     sumFWPerc += forwInfo->getforwPerc(reducedFaceId);
     const auto forwMap = forwInfo->getForwPercMap();
 
-//		std::cout << "\n";
+    //		std::cout << "\n";
     for (auto f : forwMap) {
       auto tempChangeRate = changeRate;
       auto &faceId = f.first;
@@ -54,7 +52,7 @@ public:
 
     if (sumFWPerc < 0.999 || sumFWPerc > 1.001) {
       std::cout << StrHelper::getTime() << "ERROR! Sum of fw perc out of range: " << sumFWPerc
-          << "\n";
+                << "\n";
     }
   }
 
@@ -62,10 +60,10 @@ public:
    */
   static bool
   predicate_NextHop_eligible(const Face& inFace, const Interest& interest,
-                  const fib::NextHop& nexthop,
-                  const shared_ptr<pit::Entry>& pitEntry,
-                  bool wantUnused = false,
-                  time::steady_clock::TimePoint now = time::steady_clock::TimePoint::min())
+                             const fib::NextHop& nexthop,
+                             const shared_ptr<pit::Entry>& pitEntry,
+                             bool wantUnused = false,
+                             time::steady_clock::TimePoint now = time::steady_clock::TimePoint::min())
   {
 
     const Face& outFace = nexthop.getFace();
@@ -95,9 +93,9 @@ public:
     auto tag = packet.getTag<ns3::ndn::Ns3PacketTag>();
     if (tag != nullptr) {
       ns3::Ptr<const ns3::Packet> pkt = tag->getPacket();
-      ns3::ndn::Ns3CCTag tempTag;  
+      ns3::ndn::Ns3CCTag tempTag;
       bool hasTag = pkt->PeekPacketTag(tempTag);
-//      std::cout << "Has CC Tag? " << hasTag << "\n";
+      //      std::cout << "Has CC Tag? " << hasTag << "\n";
       shared_ptr<ns3::ndn::Ns3CCTag> ns3ccTag = make_shared<ns3::ndn::Ns3CCTag>();
       if (hasTag) {
         auto it = pkt->GetPacketTagIterator();
@@ -140,13 +138,13 @@ public:
   static std::tuple<Name, MtForwardingInfo*>
   findPrefixMeasurementsLPM(const pit::Entry& pitEntry, MeasurementsAccessor& measurements)
   {
-    
-//    shared_ptr<measurements::Entry> me = measurements.get(name);
-//    shared_ptr<measurements::Entry> me = measurements.findLongestPrefixMatch(name);
+
+    //    shared_ptr<measurements::Entry> me = measurements.get(name);
+    //    shared_ptr<measurements::Entry> me = measurements.findLongestPrefixMatch(name);
     measurements::Entry* me = measurements.findLongestPrefixMatch(pitEntry);
     if (me == nullptr) {
       std::cout << "Name " << pitEntry.getName().toUri() << " not found!\n";
-      return std::forward_as_tuple(Name(), nullptr); 
+      return std::forward_as_tuple(Name(), nullptr);
     }
     MtForwardingInfo* mi = me->getStrategyInfo<MtForwardingInfo>();
     assert(mi != nullptr);
@@ -165,14 +163,14 @@ public:
 
   static bool
   isCongested(Forwarder& forwarder,
-      int faceid,
-      double thresholdRTTInMs = 1000)
+              int faceid,
+              double thresholdRTTInMs = 1000)
   {
     /* Congestion Detection by RTT */
 
     auto avgRTT = avgRTTMap[faceid]; // get average RTT for this face
     if (avgRTT > thresholdRTTInMs){
-        return true;
+      return true;
     }
     return false;
   }
@@ -186,17 +184,17 @@ public:
   }
 
   // Print table: time, node, faceid, type, fwPerc
-  static void 
+  static void
   printFwPerc(shared_ptr<std::ostream> os,
-      uint32_t nodeId,
-      std::string prefix,
-      FaceId faceId,
-      std::string type,
-      double fwPerc)
+              uint32_t nodeId,
+              std::string prefix,
+              FaceId faceId,
+              std::string type,
+              double fwPerc)
   {
     *os << ns3::Simulator::Now().ToDouble(ns3::Time::S) << "\t" << nodeId << "\t" << prefix << "\t"
         << faceId << "\t" << type << "\t" << fwPerc << "\n";
-//		os->flush();
+    //		os->flush();
   }
 
   static double
@@ -234,7 +232,7 @@ public:
       return defaultValue;
     }
   }
-  
+
   static void
   addAvgRTT(int faceid, int avgRTT)
   {
@@ -242,7 +240,7 @@ public:
   }
 
 private:
-  
+
   static std::map<int, int> avgRTTMap; // <faceid, avgRTT>
 
   static std::string
@@ -254,7 +252,7 @@ private:
 
   static void
   printFIB(Fib& fib)
-  { 
+  {
     Fib::const_iterator it = fib.begin();
     while (it != fib.end()) {
       std::cout << it->getPrefix() << ": ";
@@ -273,10 +271,12 @@ private:
       std::cout << f.getFace().getLocalUri() << f.getFace().getId() << ", ";
     }
     std::cout << "\n";
-  } 
+  }
 
 };
-}
-}
+
+
+} // namespace fw
+} // namespace nfd
 
 #endif
